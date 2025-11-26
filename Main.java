d744888a3b87bd3d766e7ee48f2c03ee2a275bad
liuxuyao <3179978785@qq.com>
2025-11-26 16:37:44 +0800

    Main add12

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main");
+        System.out.println("main1");
+        System.out.println("main2");
 
     }
 }
